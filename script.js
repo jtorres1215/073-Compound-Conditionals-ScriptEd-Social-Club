5f0b270a7f80af58389fb3460aa7f0224015b2e8
Justin T <43036766+jtorres1215@users.noreply.github.com>
2019-04-01 08:49:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,13 @@
+// set the correct username and password combination below
+var correctUsername = "";
+var correctPassword = "";
+
+
+
+$("button").click(function() {
+    var username = $(".username").val();
+    var password = $(".password").val();
+    
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW
+
+});
